fda9bd3114e7c64ff6e9b2f6b28dfc32abf4e21e
Prajun Trital <prajuncs@gmail.com>
2025-11-25 22:11:49 -0600

    push again

--- wxcbench/__init__.py
+++ wxcbench/__init__.py
@@ -2,7 +2,7 @@
 WxC-Bench: Multi-modal dataset toolkit for weather and climate downstream tasks
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Prajun Trital"
 __license__ = "MIT"
 
